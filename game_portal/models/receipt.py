3ecb472f0f5884728a3588ecff07b3dcfac3c9fa
Nirav Javiya <niravjaviya1899@gmail.com>
2025-01-02 18:31:36 +0530

    [ADD] add state, button,state color, filter,  searchview default filter

--- game_portal/models/receipt.py
+++ game_portal/models/receipt.py
@@ -3,10 +3,17 @@ from odoo import fields, models
 class receipt (models.Model):
     _name = 'gaming.receipt'
     _description = 'Gaming receipt'
-    _inherit = ['image.mixin']
+    _inherit = ['image.mixin','mail.thread', 'mail.activity.mixin']
 
     name= fields.Char(string='Name', required= True)
+    state= fields.Selection([('draft','Draft'),('approved','Approved '),('rejected','Rejected')],default='draft')
     user_id= fields.Many2one('res.users', string= 'Users')
     amount= fields.Float()
     account_id= fields.Many2one('res.partner.bank', string='Account')
+
+    def status_approved(self):
+        self.state = 'approved'
+
+    def status_rejected(self):
+        self.state = 'rejected'    
     
