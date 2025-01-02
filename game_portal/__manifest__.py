1c95bee817797bec2577c11ab97a3e3c9fbf415f
Nirav Javiya <niravjaviya1899@gmail.com>
2025-01-02 17:20:38 +0530

    [ADD] add colout to state nd filter to searchview

--- game_portal/__manifest__.py
+++ game_portal/__manifest__.py
@@ -5,7 +5,7 @@
     "description" """
            Gaming portal to show inbuilt functionalities"""
     "category": "Tools",
-    "depends": ['portal'],
+    "depends": ['portal','mail'],
     "data": [
         'security/ir.model.access.csv',
         'views/receipt_view.xml',
--- game_portal/models/player.py
+++ game_portal/models/player.py
@@ -3,7 +3,15 @@ from odoo import fields, models
 class player (models.Model):
     _name = 'gaming.player'
     _description = 'Gaming player'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name= fields.Char(string='Name', required= True)
+    name = fields.Char(string='Name', required= True)
+    state= fields.Selection([('draft','Draft'),('active','Active'),('inactive','Inactive')],default='draft')
     wallet_id= fields.Many2one('gaming.wallet', string="Wallet")
+
+    def status_active(self):
+        self.state = 'active'
+
+    def status_inactive(self):
+        self.state = 'inactive'    
     
