096fff3f3ca1e9448489395f9486834b9a7e7b86
Nirav Javiya <niravjaviya1899@gmail.com>
2025-01-03 09:49:01 +0530

    [ADD]add filter,state to subscription view

--- game_portal/models/subscription.py
+++ game_portal/models/subscription.py
@@ -3,9 +3,18 @@ from odoo import fields, models
 class subscription (models.Model):
     _name = 'gaming.subscription'
     _description = 'Gaming subscription'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
 
     name = fields.Char(string='Name', required= True)
+    state= fields.Selection([('draft','Draft'),('subscribed','Subscribed'),('pending','Pending')],default='draft')
     player_id = fields.Many2one('gaming.player',string='Players')
     product_id = fields.Many2one('product.product',string='Products')
     user_id= fields.Many2one('res.users', string= 'Users')
     transaction_ids= fields.One2many('gaming.transaction','wallet_id', string='Transactions')
+
+    def status_subscribed(self):
+        self.state = 'subscribed'
+
+    def status_pending(self):
+        self.state = 'pending'
