c87e4f689b1ec285310d0e326358daab79cd717a
Nirav Javiya <niravjaviya1899@gmail.com>
2025-01-30 10:55:12 +0530

    [ADD] add smart action using second method

--- game_portal/models/partner.py
+++ game_portal/models/partner.py
@@ -17,18 +17,18 @@ class Partner(models.Model):
 
             partner.wallet_count = self.env['gaming.wallet'].search_count([('partner_id', '=', partner.id)])
 
-    def action_show_wallets(self):
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Wallets',
-            'res_model': 'gaming.wallet',
-            'view_mode': 'list,form',
-            'domain': [('partner_id', '=', self.id)],
-            'target': 'current',
-        }
-    
     # def action_show_wallets(self):
-    #     action = self.env['ir.actions.act_window']._for_xml_id('game_portal.action_gaming_wallet')
-    #     # all_child = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-    #     action["domain"] = [('partner_id', '=', self.id)]
-    #     return action
+    #     return {
+    #         'type': 'ir.actions.act_window',
+    #         'name': 'Wallets',
+    #         'res_model': 'gaming.wallet',
+    #         'view_mode': 'list,form',
+    #         'domain': [('partner_id', '=', self.id)],
+    #         'target': 'current',
+    #     }
+    
+    def action_show_wallets(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('game_portal.action_gaming_wallet')
+        # all_child = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
+        action["domain"] = [('partner_id', '=', self.id)]
+        return action
